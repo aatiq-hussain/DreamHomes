2b5ce6bb5d8266c137069e2747cfb4c58a58ef20
aatiq-hussain <82155411+aatiq-hussain@users.noreply.github.com>
2024-04-10 21:36:49 +0530

    Update contact.js

--- Anime/contact.js
+++ Anime/contact.js
@@ -89,7 +89,34 @@ document.getElementById('option1').addEventListener('change', function() {
 
 
 
- function getParameterByName(name, url) {
+ // function getParameterByName(name, url) {
+ //      if (!url) url = window.location.href;
+ //      name = name.replace(/[\[\]]/g, '\\$&');
+ //      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+ //          results = regex.exec(url);
+ //      if (!results) return null;
+ //      if (!results[2]) return '';
+ //      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+ //    }
+
+ //    // Get the selectedOption parameter from the URL
+ //    var selectedOption = getParameterByName('selectedOption');
+
+ //    // If the selectedOption parameter is found, set the corresponding option in the select element
+ //    if (selectedOption) {
+ //      var selectElement = document.getElementById('option1');
+ //      var options = selectElement.options;
+ //      for (var i = 0; i < options.length; i++) {
+ //        if (options[i].text === selectedOption) {
+ //          options[i].selected = true;
+ //          break;
+ //        }
+ //      }
+ //    }
+
+
+ // Function to get URL parameter value by name
+    function getParameterByName(name, url) {
       if (!url) url = window.location.href;
       name = name.replace(/[\[\]]/g, '\\$&');
       var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -107,7 +134,7 @@ document.getElementById('option1').addEventListener('change', function() {
       var selectElement = document.getElementById('option1');
       var options = selectElement.options;
       for (var i = 0; i < options.length; i++) {
-        if (options[i].text === selectedOption) {
+        if (options[i].textContent.trim() === selectedOption.trim()) {
           options[i].selected = true;
           break;
         }
